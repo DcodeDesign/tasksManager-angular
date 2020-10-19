910a65d72d1dff9eb2aa8405bf000a1c0a215a7b
workspace <gravythomas@gmail.com>
2020-10-19 18:08:25 +0200

    init Projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// COMPONENTS
+import { TasksComponent } from './Components/tasks/Tasks.component';
+
+// IMPORTS
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTaskComponent } from './Components/edit-task/edit-task.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    TasksComponent,
+    AppComponent,
+    EditTaskComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
